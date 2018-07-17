903d2526983cec2643011c10c48e6423672030f7
Jonatan Vianna <jonatan.vianna@gmail.com>
2018-07-16 23:42:07 -0300

    remove print from view

--- workremotely/jobs/views.py
+++ workremotely/jobs/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .forms import JobForm
 
+
 def create_job(request):
     job_form = JobForm
     if request.method == 'POST':
         job_form = job_form(request.POST)
         if job_form.is_valid():
-            print('valid')
             job_form.save()
             job_form = JobForm()
     return render(request, 'jobs/create_job.html', {'form': job_form})
