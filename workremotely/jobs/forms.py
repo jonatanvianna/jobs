7357087ce773afb76888773e6a201570b98cb8a2
Jonatan Vianna <jonatan.vianna@gmail.com>
2018-08-09 00:51:25 -0300

    create form redesigned

--- workremotely/jobs/forms.py
+++ workremotely/jobs/forms.py
@@ -5,4 +5,21 @@ from .models import Job
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
-        fields = '__all__'
+        fields = "__all__"
+        widgets = {
+            "title": forms.TextInput(
+                attrs={"placeholder": "Python developer", "class": "input"}
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "placeholder": "Some Job description",
+                    "class": "textarea",
+                }
+            ),
+            "company": forms.TextInput(
+                attrs={"placeholder": "Acme inc.", "class": "input"}
+            ),
+            "email": forms.EmailInput(
+                attrs={"placeholder": "jobs@acme.inc", "class": "input"}
+            ),
+        }
