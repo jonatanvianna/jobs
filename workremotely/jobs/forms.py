b74113edd5712fbf53fa2a39319a4fd087a613dc
Jonatan Vianna <jonatan.vianna@gmail.com>
2018-08-09 21:59:51 -0300

    create update forms redisigned

--- workremotely/jobs/forms.py
+++ workremotely/jobs/forms.py
@@ -6,13 +6,20 @@ class JobForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = "__all__"
+        help_texts = {
+            "title": "The Job position you are creating.",
+            "description": "Fill the Job details, like tasks and skills.",
+            "company": "The company that is offering the position.",
+            "email": "The email contat for the candidates.",
+
+            }
         widgets = {
             "title": forms.TextInput(
                 attrs={"placeholder": "Python developer", "class": "input"}
             ),
             "description": forms.Textarea(
                 attrs={
-                    "placeholder": "Some Job description",
+                    "placeholder": "ACME Company is looking for a Python developer ....",
                     "class": "textarea",
                 }
             ),
